d06a994311ef59c47cbcb4973457ebf1f66cb1db
Jordan <camiloarmijosantifonamusic@gmail.com>
2022-04-26 18:20:51 -0500

    Set mode to development and clean to true

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+  mode: 'development',        
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
@@ -12,5 +13,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
